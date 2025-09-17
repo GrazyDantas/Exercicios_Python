59ae49afe8b3f3ed6f8e3e8dee33ee74d8efc108
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 21:50:43 -0300

    Atualizar o exec09.py

--- exec09.py
+++ exec09.py
@@ -1,1 +1,6 @@
-#Escreva um programa que calcule o quadrado de um número.
+#Escreva um programa que calcule o quadrado de um número.
+num = int(input("Digite um número: "))
+
+quadrado = num * num
+
+print("O quadrado de",num,"é:",quadrado)
