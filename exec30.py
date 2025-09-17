d1095d455ec10ab728c91c97f3253638ef7e3d07
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 23:10:14 -0300

    Atualizar o exec30.py

--- exec30.py
+++ exec30.py
@@ -1,1 +1,10 @@
-#Escreva um programa que classifique a nota de um aluno.
+#Escreva um programa que classifique a nota de um aluno.
+
+nota = float(input("Digite a sua nota: "))
+
+if(nota >= 7.0):
+  print("Nota boa!")
+elif (nota >= 5.0 and nota < 6.9):
+  print("Nota mediana")
+elif (nota < 5.0):
+  print("Nota baixa")
