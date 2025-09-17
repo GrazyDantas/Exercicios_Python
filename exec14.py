fb409e0ee32718118e7fa2e1677cbabca14ba3ac
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:37:35 -0300

    Atualizar o exec14.py

--- exec14.py
+++ exec14.py
@@ -1,1 +1,8 @@
 #Crie um programa que calcule o desconto de um produto.
+
+preco = float(input("Digite o preço do produto: "))
+desconto = float(input("Digite a porcentagem de desconto: "))
+
+novoPreco = preco - (preco * desconto)
+
+print("O valor do produto com desconto fica:",novoPreco)
