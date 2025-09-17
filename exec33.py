0d39772ac252b8e97b2860ee42cbad76725ab78c
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 23:12:05 -0300

    Atualizar o exec33.py

--- exec33.py
+++ exec33.py
@@ -1,1 +1,8 @@
 #Crie um programa que verifique se um número está em um intervalo.
+
+num = int(input("Digite um numero: "))
+
+if (num >= 1 and num <= 100):
+  print(num,"está no intervalo")
+else:
+  print(num,"não está no intervalo")
