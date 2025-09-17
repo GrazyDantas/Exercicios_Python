84abc5d6b3a32e422256523d96ff6b14350c4ab1
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:57:15 -0300

    Atualizar o exec21.py

--- exec21.py
+++ exec21.py
@@ -1,1 +1,11 @@
 #Escreva um programa que calcule a distância entre dois pontos.
+
+p1 = float(input("Digite o ponto 1 em cm: "))
+p2 = float(input("Digite o ponto 2 em cm: "))
+
+if (p1 > p2):
+    distancia = p1 - p2
+else:
+    distancia = p2 - p1
+
+print("A distância entre o ponto 1 e ponto 2 é:",distancia,"cm")
