2bf1866a859dedca02079c53da9eb630bf147abc
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 23:03:35 -0300

    Criar exec26.py

---
+++ exec26.py
@@ -0,0 +1,8 @@
+#26. Escreva um programa que verifique se um número é positivo ou negativo.
+
+num = int(input("Digite um número negativo ou positivo: "))
+
+if (num < 0):
+   print(num,"é negativo!")
+else:
+   print(num,"é positivo!")
