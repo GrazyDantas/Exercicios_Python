93b6365265fc88cc7638b2351799fc400f33e569
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:32:11 -0300

    Atualizar o exec13.py

--- exec13.py
+++ exec13.py
@@ -1,1 +1,5 @@
-#Escreva um programa que calcule a idade em dias.
+#Escreva um programa que calcule a idade em dias.
+
+idade = int(input("Qual é a sua idade: "))
+dias = idade * 365
+print("você têm:",dias,"dias de idade")
