75265de14e7c7899e00f461e885d52fdaaa4a353
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 23:01:12 -0300

    Atualizar o exec24.py

--- exec24.py
+++ exec24.py
@@ -1,1 +1,8 @@
-#Crie um programa que calcule o resto da divisão.
+#Crie um programa que calcule o resto da divisão.
+
+dividendo = int(input("Digite o numero do dividendo "))
+divisor = int(input("Digite o numero do divisor "))
+
+resto = dividendo % divisor
+
+print("Esta divisão tem resto:",resto)
