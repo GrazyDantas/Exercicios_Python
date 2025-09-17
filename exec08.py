72f13e6e42db1c0387100c2978265b8c0f2d3199
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 21:50:06 -0300

    Atualizar o exec08.py

--- exec08.py
+++ exec08.py
@@ -1,1 +1,8 @@
 #Crie um programa que calcule a média de três notas.
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print("A média das notas é:",media)
