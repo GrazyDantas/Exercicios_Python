e45f36c2a3368565847eb70b7b847e36d876c27b
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 23:02:03 -0300

    Atualizar o exec25.py

--- exec25.py
+++ exec25.py
@@ -1,1 +1,8 @@
 #Escreva um programa que calcule a hipotenusa de um triângulo retângulo.
+
+cateto1 = float(input("Digite o tamanho do cateto1: "))
+cateto2 = float(input("Digite o tamanho do cateto2: "))
+
+hipotenusa = ((cateto1 * cateto1) + (cateto2 * cateto2)) ** 0.5
+
+print("A hipotenusa é:",hipotenusa)
