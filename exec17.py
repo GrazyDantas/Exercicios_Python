5bde856dff2191dd7269a7a4248ad33d064007a0
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:50:09 -0300

    Atualizar o exec17.py

--- exec17.py
+++ exec17.py
@@ -1,1 +1,13 @@
-#Escreva um programa que calcule a potência de um número.
+#Escreva um programa que calcule a potência de um número.
+
+base = int(input("Digite um número: "))
+potencia = int(input("Digite o valor da potência: "))
+
+vezes = 0
+result = 1
+
+while (vezes < potencia):
+  result *= base
+  vezes += 1
+
+print("O resultado é:",result)
