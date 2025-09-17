649f67aa54dd50a50f3a5e18afeb733e5f6b23ab
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 21:49:00 -0300

    Atualizar o exec06.py

--- exec06.py
+++ exec06.py
@@ -1,1 +1,7 @@
 #Crie um programa que converta temperatura de Celsius para Fahrenheit.
+
+celcius = float(input("Digite a temperatura em Celsius: "))
+
+fahrenheit = (celcius * (9/5)) + 32
+
+print("A temperatura em Fahrenheit é:",fahrenheit)
