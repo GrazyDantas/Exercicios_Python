456aec281fb62363dbd32e8cde0b466942a8ace0
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:30:32 -0300

    Atualizar o exec12.py

--- exec12.py
+++ exec12.py
@@ -1,1 +1,11 @@
 #Crie um programa que troque os valores de duas variáveis.
+
+n1 = int(input("Digite um número inteiro: "))
+n2 = int(input("Digite outro número inteiro: "))
+print("Os números digitados em ordem foram",n1,"e",n2)
+
+t = n1
+n1 = n2
+n2 = t
+
+print("Agora a ordem é:",n1,"e",n2)
