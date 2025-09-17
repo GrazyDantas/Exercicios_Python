119424389887b520749f05460b042393738306fc
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 23:13:01 -0300

    Atualizar o exec34.py

--- exec34.py
+++ exec34.py
@@ -1,1 +1,19 @@
 #Escreva um programa que simule uma calculadora simples.
+
+print("=========== Calculadora ===========")
+n1 = float(input("Digite o primeiro número: "))
+sinal = input("Digite o sinal de cálculo exemplo: + , - , * , / : ")
+n2 = float(input("Digite segundo número: "))
+
+if (sinal == "+"):
+  result = n1 + n2
+elif (sinal == "-"):
+  result = n1 - n2
+elif (sinal == "*"):
+  result = n1 * n2
+elif (sinal == "/"):
+  result = n1 / n2
+else:
+  print("O sinal não foi digitao corretamente")
+
+print("O resultado é =",result)
