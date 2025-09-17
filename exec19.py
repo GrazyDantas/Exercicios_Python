ae607af08ce58e2654f5c09ca51c6412566f2474
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:55:40 -0300

    Atualizar o exec19.py

--- exec19.py
+++ exec19.py
@@ -1,1 +1,10 @@
 #Escreva um programa que calcule juros simples.
+
+capital = float(input("Digite o valor investido: "))
+taxa = float(input("Digite o valor da taxa de juros: "))
+tempo= float(input("Digite o tempo em meses: "))
+
+taxaJuros = capital * taxa * tempo
+capital = capital + taxaJuros
+
+print("O valor do capital em",tempo,"meses é de:",capital)
