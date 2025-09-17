117bfda0b870a9aae687ad6465a2ebb084872cbc
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:56:21 -0300

    Atualizar o exec20.py

--- exec20.py
+++ exec20.py
@@ -1,1 +1,6 @@
 #Crie um programa que concatene strings.
+
+strings = str(input("Digite uma palavra: "))
+vezes = int(input("Digite a quantidade de vezes que será repetido: "))
+
+print(strings*vezes)
