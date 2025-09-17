7c517dfc1da0c25b11ed15e182a9849e547787cf
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 23:08:32 -0300

    Atualizar o exec27.py

--- exec27.py
+++ exec27.py
@@ -1,1 +1,8 @@
 #Crie um programa que verifique se uma pessoa é maior de idade.
+
+idade = int(input("Digite a sua idade: "))
+
+if(idade >= 18):
+  print("Maior de idade!")
+else:
+  print("Menor de idade!")
