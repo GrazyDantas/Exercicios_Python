f722578d14a8be6c7e1cc6d6ae1fa3c42bad4929
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:42:00 -0300

    Atualizar o exec16.py

--- exec16.py
+++ exec16.py
@@ -1,1 +1,10 @@
 #Crie um programa que converta segundos em horas, minutos e segundos.
+
+segundos = int(input("Digite uma quantidade de segundos: "))
+
+minutos = segundos / 60
+horas = minutos / 60
+
+print("Segundos:",segundos)
+print("Minutos:",minutos)
+print("Horas:%.2f"%horas)
