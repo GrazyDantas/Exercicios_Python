bce0aacb6f72d0dc26c82746199476c5e9f21744
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 23:14:03 -0300

    Atualizar o exec35.py

--- exec35.py
+++ exec35.py
@@ -1,1 +1,12 @@
 #Crie um programa que determine o tipo de triângulo.
+
+l1 = int(input("Digite o prmeiro lado do triangulo: "))
+l2 = int(input("Digite o segundo lado do triangulo: "))
+l3 = int(input("Digite o terceiro lado do triangulo: "))
+
+if (l1 == l2 & l2 == l3 & l1 == l3):
+  print("Triângulo Equilátero")
+elif (l1 != l2 & l2 != l3 & l3 != l1):
+  print("Triângulo Escaleno")
+else:
+  print("Triângulo isóceles")
