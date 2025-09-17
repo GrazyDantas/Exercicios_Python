d13ca16e28c53ac77498c586a1bc14b998a2e4e9
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 21:49:35 -0300

    Atualizar o exec07.py

--- exec07.py
+++ exec07.py
@@ -1,1 +1,8 @@
 #Escreva um programa que calcule o perímetro de um círculo.
+
+diametro = float(input("Digite o diametro do circulo: "))
+
+raio = diametro / 2
+perimetro = (2 * 3.14) * raio
+
+print("O perimetro do circulo é:",perimetro)
