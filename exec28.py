2f912fb55e3ac467e54ad314ba9d5bbaae48874c
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 23:09:09 -0300

    Atualizar o exec28.py

--- exec28.py
+++ exec28.py
@@ -1,1 +1,8 @@
 #Escreva um programa que verifique se um número é par ou ímpar.
+
+num = int(input("Digite um número: "))
+
+if (num % 2 == 0):
+  print(num,"é par!")
+else:
+  print(num,"é impar!")
