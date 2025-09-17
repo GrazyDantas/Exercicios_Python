8d7ad986847463a17ce86debc45f58ac4ecd8b31
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 21:51:29 -0300

    Atualizar o exec10.py

--- exec10.py
+++ exec10.py
@@ -1,1 +1,11 @@
 #Crie um programa que exiba os tipos de dados de diferentes variáveis.
+
+num = 10
+nome = 'grazy'
+flu = 3.14
+vf = True
+print(type(10))
+print(type(nome))
+print(type(3.14))
+print(type(true))
+
