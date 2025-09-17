ef303fb4484e42ed7af130d1946854e2e41ec4b1
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 21:13:00 -0300

    Criar exec05.py

---
+++ exec05.py
@@ -0,0 +1,8 @@
+# Escreva um programa que calcule a área de um retângulo.
+
+base = int(input("Digite o valor da base:"))
+altura = int(input("Digite a altura:"))
+
+area = base * altura
+
+print("A area deste retângulo é:",area)
