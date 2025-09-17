7ca66017a68f895b187375532c617b5bc8e712be
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:38:21 -0300

    Atualizar o exec15.py

--- exec15.py
+++ exec15.py
@@ -1,1 +1,8 @@
 #Escreva um programa que calcule a área de um triângulo.
+
+base = int(input("Digite o valor da base do triângulo: "))
+altura = int(input("Digite a altura do triângulo"))
+
+area = (base * altura) / 2
+
+print("A area do triângulo é:",area)
