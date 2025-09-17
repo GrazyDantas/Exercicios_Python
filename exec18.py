06d334c47f4d4e03567d53284c47244373853200
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:54:37 -0300

    Atualizar o exec18.py

--- exec18.py
+++ exec18.py
@@ -1,1 +1,8 @@
 #Crie um programa que calcule a velocidade média.
+
+horas = float(input("Digite o tempo em horas: "))
+quilometros = float(input("Digite a quilometragem percorrida: "))
+
+velMedia = quilometros / horas
+
+print("A velocidade média é:%.2f"%velMedia)
