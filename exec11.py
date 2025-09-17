d9d4f2724d7367aeeab18b5845c59c7d4120de55
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:28:47 -0300

    Atualizar o exec11.py

--- exec11.py
+++ exec11.py
@@ -1,1 +1,8 @@
 #Escreva um programa que calcule o IMC (Índice de Massa Corporal).
+
+peso = float(input("Digite o seu peso em Kilos: "))
+altura = float(input("Digite a sua altura em centimetros: ")
+
+imc = peso / (altura * altura)
+
+print("O seu imc é:",imc)
