43054e74d930b55416091bc7c2775f6cbb32b15d
GяаzyDаитаs <166925345+GrazyDantas@users.noreply.github.com>
2025-09-16 22:59:09 -0300

    Atualizar o exec22.py

--- exec22.py
+++ exec22.py
@@ -1,1 +1,8 @@
-#Crie um programa que calcule o volume de uma esfera.
+#Crie um programa que calcule o volume de uma esfera.
+
+raio = float(input("Digite o raio da esfera: "))
+
+raio = raio * raio * raio
+volume =  raio * 3.14 * (4 / 3)
+
+print("O volume da esfera é:#.2f"#volume)
